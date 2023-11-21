5a9d1f0b49162cb413aea1a27683e2663c872814
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2023-11-22 02:05:47 +0700

    Update osk import for manual import

--- scripts/InputBar.js
+++ scripts/InputBar.js
@@ -8,13 +8,18 @@ function mapInputEnter(e) {
 
 document.querySelector("#map-dropper").onchange = () => {
     const file = document.querySelector("#map-dropper").files[0];
-    if (!["osz", "osr"].includes(file.name.split(".").at(-1))) return;
+    if (!["osz", "osr", "osk"].includes(file.name.split(".").at(-1))) return;
 
     ScoreParser.reset();
 
     if (file.name.split(".").at(-1) === "osr") {
         const parser = new ScoreParser(file);
         parser.getReplayData();
+        return;
+    }
+
+    if (file.name.split(".").at(-1) === "osk") {
+        Skinning.import(file);
         return;
     }
 
