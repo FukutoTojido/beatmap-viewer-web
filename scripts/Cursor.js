e7dc8e95479683dd847dfcffb5e9838f8231aca3
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2025-05-01 16:25:59 +0700

    fix: cursor size being unconsistent on different screen size

--- scripts/Cursor.js
+++ scripts/Cursor.js
@@ -1,65 +1,75 @@
 import { Game } from "./Game.js";
 import { ScoreParser } from "./ScoreParser.js";
 import * as PIXI from "pixi.js";
-import {loadParallel} from "./Utils.js";
+import { loadParallel } from "./Utils.js";
+
+const CURSOR_SIZE = 0.3;
 
 export class Cursor {
-    obj;
-    cursor;
-    trailList;
+	obj;
+	cursor;
+	trailList;
 
-    async init() {
-        const container = new PIXI.Container();
+	async init() {
+		const container = new PIXI.Container();
 
-        const [cursorTexture, trailTexture] = await Promise.all([
-            PIXI.Assets.load("/static/cursor.png"),
-            PIXI.Assets.load("/static/cursortrail.png")
-        ]);
-        this.cursor = PIXI.Sprite.from(cursorTexture);
-        this.cursor.anchor.set(0.5);
-        this.cursor.width = (128 * (1024 / 640)) / (Game.WIDTH / 512);
-        this.cursor.height = (128 * (1024 / 640)) / (Game.WIDTH / 512);
+		const [cursorTexture, trailTexture] = await Promise.all([
+			PIXI.Assets.load("/static/cursor.png"),
+			PIXI.Assets.load("/static/cursortrail.png"),
+		]);
+		this.cursor = PIXI.Sprite.from(cursorTexture);
+		this.cursor.anchor.set(0.5);
+		this.cursor.width = 128 * (1024 / 640);
+		this.cursor.height = 128 * (1024 / 640);
+		this.cursor.scale.set(CURSOR_SIZE);
 
-        const trailList = [...Array(10).keys()].map(() => PIXI.Sprite.from(trailTexture));
-        this.trailList = trailList;
+		const trailList = [...Array(10).keys()].map(() =>
+			PIXI.Sprite.from(trailTexture),
+		);
+		this.trailList = trailList;
 
-        trailList.forEach((cursor) => {
-            cursor.anchor.set(0.5);
-            cursor.width = (64 * (1024 / 640)) / (Game.WIDTH / 512);
-            cursor.height = (64 * (1024 / 640)) / (Game.WIDTH / 512);
-            container.addChild(cursor);
-        });
-        container.addChild(this.cursor);
-        container.x = Game.OFFSET_X;
-        container.y = Game.OFFSET_Y;
+		trailList.forEach((cursor) => {
+			cursor.anchor.set(0.5);
+			cursor.width = 64 * (1024 / 640);
+			cursor.height = 64 * (1024 / 640);
+			cursor.scale.set(CURSOR_SIZE);
+			container.addChild(cursor);
+		});
+		container.addChild(this.cursor);
+		container.x = Game.OFFSET_X;
+		container.y = Game.OFFSET_Y;
 
-        this.obj = container;
-        this.obj.alpha = 0;
-    }
+		this.obj = container;
+		this.obj.alpha = 0;
+	}
 
-    constructor() {
+	constructor() {}
 
-    }
+	update(index, current_x, current_y) {
+		if (!ScoreParser.CURSOR_DATA) return;
+		this.obj.x = Game.OFFSET_X;
+		this.obj.y = Game.OFFSET_Y;
+		this.obj.scale.set(Game.SCALE_RATE);
 
-    update(index, current_x, current_y) {
-        if (!ScoreParser.CURSOR_DATA) return;
-        this.obj.x = Game.OFFSET_X + current_x * (Game.WIDTH / 512);
-        this.obj.y = Game.OFFSET_Y + current_y * (Game.WIDTH / 512);
+		this.cursor.x = current_x;
+		this.cursor.y = current_y;
+		this.cursor.scale.set(CURSOR_SIZE);
 
-        index--;
+		index--;
 
-        this.trailList.toReversed().forEach((graphic, graphic_idx) => {
-            if (index < 0) return;
+		this.trailList.toReversed().forEach((graphic, graphic_idx) => {
+			if (index < 0) return;
 
-            const x_delta = (ScoreParser.CURSOR_DATA[index].x - current_x) * (Game.WIDTH / 512);
-            const y_delta = (ScoreParser.CURSOR_DATA[index].y - current_y) * (Game.WIDTH / 512);
+			const x_delta = ScoreParser.CURSOR_DATA[index].x;
+			const y_delta = ScoreParser.CURSOR_DATA[index].y;
 
-            graphic.x = x_delta;
-            graphic.y = y_delta;
-            graphic.alpha = 1 - graphic_idx / this.trailList.length;
+			graphic.x = x_delta;
+			graphic.y = y_delta;
+			graphic.scale.set(CURSOR_SIZE * 2);
+			graphic.alpha = 1 - graphic_idx / this.trailList.length;
 
-            index--;
-            return;
-        });
-    }
+			index--;
+			return;
+		});
+	}
 }
