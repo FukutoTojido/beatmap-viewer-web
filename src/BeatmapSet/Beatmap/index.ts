8674519a1917bef796c7c45c686c4bdc1e42ad99
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2025-05-08 20:47:04 +0700

    fix: remove the need of requestAnimationFrame

--- src/BeatmapSet/Beatmap/index.ts
+++ src/BeatmapSet/Beatmap/index.ts
@@ -295,7 +295,7 @@ export default class Beatmap extends ScopedClass {
 			.sort((a, b) => -a.object.startTime + b.object.startTime);
 
 		for (const object of sorted) {
-			requestAnimationFrame(() => object.update(this.audio?.currentTime ?? 0));
+			object.update(this.audio?.currentTime ?? 0);
 			object.playHitSound(time);
 			containers.push(object.container);
 
