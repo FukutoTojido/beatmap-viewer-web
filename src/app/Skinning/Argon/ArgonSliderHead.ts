d81bb5802303130ced6bcee9a91032daed232c93
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2025-10-31 16:23:44 +0700

    fix: discord mess

--- src/app/Skinning/Argon/ArgonSliderHead.ts
+++ src/app/Skinning/Argon/ArgonSliderHead.ts
@@ -5,17 +5,20 @@ import { inject } from "@/Context";
 export const update = (drawable: DrawableSliderHead, time: number) => {
 	const startTime = drawable.evaluation?.hitTime ?? drawable.object.startTime;
 
-	if (time < startTime) {
+	if (
+		time < startTime ||
+		!inject<GameplayConfig>("config/gameplay")?.hitAnimation
+	) {
 		const skin = drawable.skinManager?.getCurrentSkin();
 		const sliderStartCircle = skin?.getTexture("sliderstartcircle");
 		const hitCircle = skin?.getTexture("hitcircle");
 
 		const baseTexture = sliderStartCircle ?? hitCircle;
 		if (baseTexture) drawable.hitCircleSprite.texture = baseTexture;
+	}
 
-		if (!inject<GameplayConfig>("config/gameplay")?.hitAnimation) {
-			return;
-		}
+	if (!inject<GameplayConfig>("config/gameplay")?.hitAnimation) {
+		return;
 	}
 
 	if (time >= startTime) {
