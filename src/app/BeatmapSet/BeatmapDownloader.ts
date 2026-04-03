fe39126aa6d9ad568124b6681cdd1d0fb2e893c9
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2026-04-03 16:43:11 +0700

    feat: add hidden akatsuki beatmap mirror

--- src/app/BeatmapSet/BeatmapDownloader.ts
+++ src/app/BeatmapSet/BeatmapDownloader.ts
@@ -149,6 +149,11 @@ const fetchBlobFromMirror = async (
 	const sortedMirrors = [
 		allMirrors[configIndex],
 		...allMirrors.slice(0, configIndex),
+		{
+			url: "https://beatmaps.akatsuki.gg/api/d/$setId",
+			name: "Akatsuki",
+			rank: -1,
+		},
 		...allMirrors.slice(configIndex + 1),
 	];
 
